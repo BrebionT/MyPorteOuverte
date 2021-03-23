725f4412b3b8d71e9a4d77d3facc3c1b23aeafac
dubosqN <noe.dubosq.allende@gmail.com>
2021-03-23 11:46:08 +0100

    Correction conflits

--- app/src/main/java/com/example/mytest/Inscription.java
+++ app/src/main/java/com/example/mytest/Inscription.java
@@ -2,7 +2,9 @@ package com.example.mytest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Inscription extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class Inscription extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inscription);
+    }
+
+    public void ChangeActivityAccueil(View view){ //action de la vue
+        //pour passer d'une activité à une autre, nous utilisons un Intent
+        Intent intent = new Intent(this, Accueil.class);
+        startActivity(intent);
+
     }
 }
