bb9cf1c44b693c7d0fbf4e322a5ab45b7a1a5e43
ValentineD14 <duprez-valentine@outlook.fr>
2021-03-30 12:08:46 +0200

    Ajouts pages stats etc.

--- app/src/main/java/com/example/mytest/ListeCandidat.java
+++ app/src/main/java/com/example/mytest/ListeCandidat.java
@@ -6,17 +6,24 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class Enseignant extends AppCompatActivity {
+public class ListeCandidat extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_enseignant);
+        setContentView(R.layout.activity_liste_candidat);
     }
 
     public void ChangeActivityStatistique(View view){ //action de la vue
         //pour passer d'une activité à une autre, nous utilisons un Intent
         Intent intent = new Intent(this, Statistique.class);
+        startActivity(intent);
+
+    }
+
+    public void ChangeActivityAccueil(View view){ //action de la vue
+        //pour passer d'une activité à une autre, nous utilisons un Intent
+        Intent intent = new Intent(this, Accueil.class);
         startActivity(intent);
 
     }
--- app/src/main/java/com/example/mytest/Statistique.java
+++ app/src/main/java/com/example/mytest/Statistique.java
@@ -6,24 +6,24 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class Accueil extends AppCompatActivity {
+public class Statistique extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_accueil);
+        setContentView(R.layout.activity_statistique);
     }
 
-    public void ChangeActivityInscription(View view){ //action de la vue
+    public void ChangeActivityListeCandidat(View view){ //action de la vue
         //pour passer d'une activité à une autre, nous utilisons un Intent
-        Intent intent = new Intent(this, Inscription.class);
+        Intent intent = new Intent(this, ListeCandidat.class);
         startActivity(intent);
 
     }
 
-    public void ChangeActivityEnseignant(View view){ //action de la vue
+    public void ChangeActivityAccueil(View view){ //action de la vue
         //pour passer d'une activité à une autre, nous utilisons un Intent
-        Intent intent = new Intent(this, Enseignant.class);
+        Intent intent = new Intent(this, Accueil.class);
         startActivity(intent);
 
     }
